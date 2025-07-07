06ff7fbf485b8d36f3ecd454fcadd478eab8a1de
Md-Mahir-Asef <mdmahirasef2008@gmail.com>
2025-07-07 17:45:08 +0600

    Created Basic Home Page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+
 function Home() {
+  // const [data, setData] = useState({});
+  // const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    chrome.storage.local.get(null, (items) => {
+      // setData(items);
+      console.log(items);
+    });
+  }, []);
+  // if (isLoading) return <p>Loading...</p>
   return <h1>Home Page</h1>;
 }
 
